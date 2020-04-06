9401df4d3131a586980b38da1ff3b84934e58fed
wenjianzhang <zwj777@live.com>
2020-04-06 22:52:29 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f "https://${access_token}@github.com:wenjianzhang/go-admin-site.git" master:gh-pages
+git push -f "https://${access_token}@github.com/wenjianzhang/go-admin-site.git" master:gh-pages
 
 cd -
